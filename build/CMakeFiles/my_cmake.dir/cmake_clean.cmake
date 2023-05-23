b2b1f78fd492bcbb888f5387df35f31da0b505fd
debug <maxl@github.com>
2023-05-23 08:44:56 +0000

    “第一次提交”

---
+++ build/CMakeFiles/my_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_cmake.dir/src/main.c.o"
+  "my_cmake.pdb"
+  "my_cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    double val=1210121.9;
+    int tem=log2(val);
+    printf("%d\n",tem);
+    return 0;
+}
